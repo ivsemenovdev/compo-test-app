b60e9b96fea390d3ec5a994bc05f091b6f7d7aee
Ivan Semenov <ivsemenovdev@gmail.com>
2025-06-23 14:49:02 +0300

    add product page component, add page template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductPageComponent } from "./components/product-page/product-page.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [ProductPageComponent]
 })
 export class AppComponent {
 }
---
+++ src/app/components/product-page/product-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-page',
+  standalone: true,
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.scss'
+})
+export class ProductPageComponent {} 
